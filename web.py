d23d8215569bda0ae600f86253ced18f1691eab9
Ashish5536 <ashishkumar097gkp@gmail.com>
2025-07-31 13:14:05 +0530

    Update web.py

--- web.py
+++ web.py
@@ -2,3 +2,10 @@ import streamlit as st
 name = st.text_input("Enter your name: ")
 st.header("Python")
 st.subheader("code:1")
+st.code("""if __name__ == '__main__':
+    n = int(input())                        # Read number of scores
+    arr = list(map(int, input().split()))  # Read scores and convert to list of integers
+    unique_scores = list(set(arr))         # Remove duplicates
+    unique_scores.sort(reverse=True)       # Sort in descending order
+    print(unique_scores[1])                # Print the second highest score
+""")
